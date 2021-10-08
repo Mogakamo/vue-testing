119bab077f4ad86f3f45507025f9f04a15df06c3
Mogakamo <mogaka.amo254@gmail.com>
2021-10-08 09:49:05 +0000

    async function working

--- functions.js
+++ functions.js
@@ -1,3 +1,5 @@
+const { default: axios } = require("axios");
+
 const functions = {
     add: (num1, num2) => num1 + num2,
     isNull: () => null,
@@ -6,7 +8,10 @@ const functions = {
         const user = { firstName: 'Amos' }
         user['lastName'] = 'Mogaka';
         return user;
-    }
+    },
+    fetchUsers: () => axios.get('https://jsonplaceholder.typicode.com/users/1')
+        .then(response => response.data)
+        .catch(err => 'error')
 }
 
 module.exports = functions
--- functions.test.js
+++ functions.test.js
@@ -52,3 +52,12 @@ test('Admin should be in usernames', () => {
     usernames = ['john', 'karen', 'admin']
     expect(usernames).toContain('admin')
 })
+
+//working async data
+test('User fetched name should be Leanne Graham', () => {
+    expect.assertions(1)
+    return functions.fetchUsers()
+        .then(data => {
+            expect(data.name).toEqual('Leanne Graham')
+        })
+});
