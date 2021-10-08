46b58ca8eab350d3ef324f3c7432fbbc3d42e2e0
Mogakamo <mogaka.amo254@gmail.com>
2021-10-08 08:33:19 +0000

    basics of jest

--- functions.test.js
+++ functions.test.js
@@ -1,10 +1,12 @@
 const { test, expect } = require('@jest/globals');
 const functions = require('./functions');
 
+//equals
 test('Adds 2 + 2 to equal 4', () => {
     expect(functions.add(2, 2)).toBe(4);
 })
 
+//not
 test('Adds 2 + 2 to NOT equal 5', () => {
     expect(functions.add(2, 2)).not.toBe(5);
 })
@@ -15,11 +17,38 @@ test('Adds 2 + 2 to NOT equal 5', () => {
 //toBeDefined is the opposite of toBeUndefined
 //toBeTruthy matches anything that an if statement treats as true
 //toBeFalsy matches anything that an if statement treats as false
-
+//toBeNull
 test('Should be nulll', () => {
     expect(functions.isNull()).toBeNull();
 })
 
+//toBeFalsy
 test('Should be falsy', () => {
     expect(functions.checkValue(2)).not.toBeFalsy();
+})
+
+//toEqual
+test('User should be Amos Mogaka object', () => {
+    expect(functions.createUser()).toEqual({
+        firstName: 'Amos',
+        lastName: 'Mogaka'
+    });
+});
+
+//less than and greater than
+test('Should be under 1600', () => {
+    const loadOne = 800;
+    const loadTwo = 800;
+    expect (loadOne + loadTwo).toBeLessThanOrEqual(1600)
+})
+
+// Regex - Regular expressions 
+test('There is no I in team', () => {
+    expect('team').not.toMatch(/I/i)
+})
+
+//Arrays
+test('Admin should be in usernames', () => {
+    usernames = ['john', 'karen', 'admin']
+    expect(usernames).toContain('admin')
 })
